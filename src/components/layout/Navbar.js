48862d57b7cb1e99276c4a40601fa7d50ccae12e
Rob Peschke <rspesc31@gmail.com>
2020-03-21 16:47:59 -0500

    added static proptypes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
   static defaultProps = {
@@ -6,7 +7,10 @@ export class Navbar extends Component {
     icon: 'fab fa-github'
   };
 
-  
+  static propTypes ={
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
   render() {
     return (
       <div>
