9f3757574ef188b382ad9077b49ffdb1c4e81f55
Rob Peschke <rspesc31@gmail.com>
2020-03-21 17:42:40 -0500

    added axios get request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users:[],
+    loading: false
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users')
+    .then(res =>{
+      this.setState({
+        users: res.data,
+        loading: false
+      })
+    })
+  }
+
   render(){
     return (
       <div className="App">
