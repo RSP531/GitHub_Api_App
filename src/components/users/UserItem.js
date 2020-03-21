8f3b4c6e364cc72754b943da4787f293931bffca
Rob Peschke <rspesc31@gmail.com>
2020-03-21 17:10:50 -0500

    going to refactor to take in multiple users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -17,7 +17,6 @@ class UserItem extends Component {
         style={{width: '60px'}}
         />
         <h3>{login}</h3>
-
         <div>
           <a href={html_url} className="btn btn-dark btn-sm my-1">MORE</a>
         </div>
