8464d5cff269120c0bce5249503e2f0ddceb7fe1
Rob Peschke <rspesc31@gmail.com>
2020-03-21 18:18:00 -0500

    added onchange and onsubmit for search

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
   state = {
     users:[],
-    loading: false
+    loading: false,
   }
 
   async componentDidMount(){
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
 export class Search extends Component {
+  state = {
+    text: ''
+  }
+  onChange=(e)=>{
+    this.setState({ [e.target.name]: e.target.value})
+  }
+  onSubmit=(e)=>{
+    e.preventDefault()
+    console.log(this.state.text)
+  }
   render() {
     return (
       <div>
-        <form className="form">
-          <input type="text" name="text" placeholder="Search Github Users..."/>
-          <input type="Submit" value="Search" className="btn btn-dark btn-block"/>
+        <form onSubmit={this.onSubmit} className="form">
+          <input type="text" name="text" placeholder="Search Github Users..." value={this.state.text} onChange={this.onChange}/>
+          <input type="submit" value="Search" className="btn btn-dark btn-block"/>
         </form>
       </div>
     )
