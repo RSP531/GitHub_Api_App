bddecafe1f8ccc0502de0a337c52e0b05eb06e43
Rob Peschke <rspesc31@gmail.com>
2020-03-21 18:23:19 -0500

    added search users function in app

--- src/App.js
+++ src/App.js
@@ -24,12 +24,16 @@ class App extends Component {
     })
   }
 
+  searchUsers = (text) => {
+    console.log(text)
+  }
+
   render(){
     return (
       <div className="App">
         <Navbar/>
         <div className="container">
-        <Search/>
+        <Search searchUsers={this.searchUsers}/>
         <Users users={this.state.users} loading={this.state.loading}/>
         </div>
       </div>
