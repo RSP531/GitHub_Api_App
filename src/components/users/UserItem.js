989837ef4828bc5e618e08a7c3bf1c1afdfa5176
Rob Peschke <rspesc31@gmail.com>
2020-03-21 17:09:35 -0500

    got rid of constructor, super and deconstructed state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor(){
-    super()
-      this.state ={
+    state = {
         id: 'id',
         login: 'RSP531',
         avatar_url: 'https://avatars3.githubusercontent.com/u/50122770?v=4',
         html_url: 'https://github.com/RSP531'
       }
-  }
   render() {
+    const {login, html_url, avatar_url} = this.state;
     return (
       <div className="card text-center">
         <img 
-        src ={this.state.avatar_url} 
+        src ={avatar_url} 
         alt="" className="round-img" 
         style={{width: '60px'}}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
 
         <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">MORE</a>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">MORE</a>
         </div>
       </div>
     )
