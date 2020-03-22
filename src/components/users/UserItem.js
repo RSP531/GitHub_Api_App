601ade4a045bf1024a38d85c63c240711bd02de4
Rob Peschke <rspesc31@gmail.com>
2020-03-22 13:20:19 -0500

    added individual bio page

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 const UserItem = ({user: {login, html_url, avatar_url}}) => {
 
@@ -12,7 +13,7 @@ const UserItem = ({user: {login, html_url, avatar_url}}) => {
         />
         <h3>{login}</h3>
         <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">MORE</a>
+          <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">MORE</Link>
         </div>
       </div>
     )
