03424f48134cd21f263c3add8d40fce01a8b9896
Rob Peschke <rspesc31@gmail.com>
2020-03-22 13:59:59 -0500

    getting prettier to work on save

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types'; //makes app more robust to check datatype
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types"; //makes app more robust to check datatype
+import { Link } from "react-router-dom";
 
-const Navbar = ({icon, title}) => {
-    return (
-      <div className="navbar bg-primary">
-        <h1>
-        <i className={icon}/>
-          {title}
-        </h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
+const Navbar = ({ icon, title }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
           <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </div>
-    )
-}
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 Navbar.defaultProps = {
-  title: 'Github Finder',
-  icon: 'fab fa-github'
+  title: "Github Finder",
+  icon: "fab fa-github"
 };
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired
-}
-export default Navbar
+};
+export default Navbar;
